74a73c46cfb683de0121530dbbbe5b866ac8cce7
Patrick Liang <pl204511194@yahoo.com>
2022-04-05 11:59:45 -0400

    commit tree af364b17926947a05f5d71aebcfa199833f3c9e8

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let string = ""
+  for (i=str.length-1; i>=0; i--) {
+      string+=str[i]
+  }
+  return string
 }
 
 if (require.main === module) {
@@ -17,3 +22,8 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// initialize an empty array
+// iterate over inputted string from the end to the beginning of each letter 
+// push the reversed letters to the empty array 
+// join the array into a string
